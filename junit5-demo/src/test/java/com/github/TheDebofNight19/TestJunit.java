0d5d961ddf7f07ac174c9a46fc401f682c8fa798
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-08 14:54:18 +0300

    fixtures added;

--- junit5-demo/src/test/java/com/github/TheDebofNight19/TestJunit.java
+++ junit5-demo/src/test/java/com/github/TheDebofNight19/TestJunit.java
@@ -1,5 +1,5 @@
 package com.github.TheDebofNight19;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,9 +7,36 @@ public class TestJunit {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestJunit.class);
 
+    @BeforeAll
+    static void beforeAll(){
+        LOG.info("This comes before ALL");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        LOG.info("This comes before EACH");
+    }
+
     @Test
     void testMethod1(){
-        LOG.info("The test is complete.");
+        LOG.info("The first test is complete.");
 
     }
+
+    @Test
+    void testMethod2(){
+        LOG.info("The second test is complete.");
+
+    }
+
+    @AfterEach
+    void afterEach(){
+        LOG.info("This comes after EACH");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        LOG.info("This comes after ALL");
+    }
+
 }
